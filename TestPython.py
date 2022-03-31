4b7c763aaacf30206e70cac9a8965c310ede19ac
Teck Tran <trant3@icloud.com>
2022-03-31 18:38:05 -0400

    initial

--- TestPython.py
+++ TestPython.py
@@ -1,5 +1,14 @@
-myList = [2, 6, 9, 20]
+class Car():
+    def __init__(self, make, color):
+        self.make = make
+        self.color = color
 
-myList.append(21)
-myList.remove(2)
-print(myList)
+    def display_msg(self):
+        print("Car is {color} and is a {make}".format(color = self.color, make = self.make))
+
+car1 = Car("Tesla", "white")
+car1.display_msg()
+
+car2 = Car("Honda", "blue")
+car2.display_msg()
+
